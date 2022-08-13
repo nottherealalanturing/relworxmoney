ca3cffbaddb9de9289fd8b11425d2340ab410c98
Assad Isah <nottherealalanturing@gmail.com>
2022-08-14 00:29:51 +0100

    feat: add validation for unique users

--- assets/script/signup.js
+++ assets/script/signup.js
@@ -17,27 +17,11 @@ signupForm.addEventListener("submit", (e) => {
     balance: 0,
     account_number: users.length + 1,
   };
-  users.push(formdata);
-  localStorage.setItem("userslist", JSON.stringify(users));
-});
-
-signinForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  (email = username_signin.value),
-    (password = password_signin.value),
-    (users = JSON.parse(localStorage.getItem("userslist")) || []);
   users.forEach((user) => {
-    if (user.email === email && user.password === password) {
-      localStorage.setItem(
-        "userloggedin",
-        JSON.stringify({
-          username: user.email,
-          password: user.password,
-          balance: user.balance,
-          fullname: user.fullname,
-          account_number: user.account_number,
-        })
-      );
+    if (user.email !== formdata.email) {
+      users.push(formdata);
+      localStorage.setItem("userslist", JSON.stringify(users));
+      window.location.replace("/pages/signin.html");
     }
   });
 });

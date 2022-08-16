472034ac4c12e44d425c40d0f84e6e63959f4a1d
Assad Isah <nottherealalanturing@gmail.com>
2022-08-16 13:29:56 +0100

    fix: resolve errors with linters

--- assets/script/signin.js
+++ assets/script/signin.js
@@ -1,64 +1,64 @@
-const signinForm = document.querySelector(".form_signin");
-const username_signin = document.getElementById("username_signin");
-const password_signin = document.getElementById("password_signin");
-let currentuser = JSON.parse(localStorage.getItem("userloggedin"));
+const signinForm = document.querySelector('.form_signin');
+const usernameSignin = document.getElementById('username_signin');
+const passwordSignin = document.getElementById('password_signin');
+const currentuser = JSON.parse(localStorage.getItem('userloggedin'));
 
-signinForm.addEventListener("submit", (e) => {
+signinForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  email = username_signin.value;
-  password = password_signin.value;
-  users = JSON.parse(localStorage.getItem("userslist")) || [];
+  const email = usernameSignin.value;
+  const password = passwordSignin.value;
+  const users = JSON.parse(localStorage.getItem('userslist')) || [];
 
-  if (email === "admin" && password === "admin") {
+  if (email === 'admin' && password === 'admin') {
     localStorage.setItem(
-      "userloggedin",
+      'userloggedin',
       JSON.stringify({
-        username: "admin",
-        password: "admin",
-        balance: "infinite",
-        fullname: "admin",
-        account_number: "0",
-        role: "admin",
-      })
+        username: 'admin',
+        password: 'admin',
+        balance: 'infinite',
+        fullname: 'admin',
+        account_number: '0',
+        role: 'admin',
+      }),
     );
-    window.location.replace("/pages/admindashboard.html");
+    window.location.replace('/pages/admindashboard.html');
     return;
   }
 
   users.forEach((user) => {
     if (user.email === email && user.password === password) {
       localStorage.setItem(
-        "userloggedin",
+        'userloggedin',
         JSON.stringify({
           username: user.email,
           password: user.password,
           balance: user.balance,
           fullname: user.fullname,
           account_number: user.account_number,
-          role: "user",
-        })
+          role: 'user',
+        }),
       );
-      window.location.replace("/pages/userdashboard.html");
+      window.location.replace('/pages/userdashboard.html');
     }
   });
 });
 
-window.addEventListener("load", function () {
-  const userNav = document.querySelector(".user-nav");
-  const adminNav = document.querySelector(".admin-nav");
-  const noneUser = document.querySelector(".nu-nav");
+window.addEventListener('load', () => {
+  const userNav = document.querySelector('.user-nav');
+  const adminNav = document.querySelector('.admin-nav');
+  const noneUser = document.querySelector('.nu-nav');
 
-  if (currentuser.role === "user") {
-    userNav.classList.remove("hide");
-    adminNav.classList.add("hide");
-    noneUser.classList.add("hide");
-  } else if (currentuser.role === "admin") {
-    userNav.classList.add("hide");
-    adminNav.classList.remove("hide");
-    noneUser.classList.add("hide");
+  if (currentuser.role === 'user') {
+    userNav.classList.remove('hide');
+    adminNav.classList.add('hide');
+    noneUser.classList.add('hide');
+  } else if (currentuser.role === 'admin') {
+    userNav.classList.add('hide');
+    adminNav.classList.remove('hide');
+    noneUser.classList.add('hide');
   } else {
-    userNav.classList.add("hide");
-    adminNav.classList.add("hide");
-    noneUser.classList.remove("hide");
+    userNav.classList.add('hide');
+    adminNav.classList.add('hide');
+    noneUser.classList.remove('hide');
   }
 });
